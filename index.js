856cdc465e0280a6819890f06e52e10cd545ebe1
hobbito <cristhianlluncor@gmail.com>
2021-09-18 11:14:18 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+async function doTask() {
+    let launching = new Promise((resolve) => {
+        let listNumber = [];
+        let intent = 11;
+        while (intent > 0) {
+            let number = Math.trunc(Math.random() * (7 - 1) + 1);
+            if (number <= 6) {
+                listNumber.push(number)
+            } else {
+                return ;
+            }
+            intent --;
+            resolve(listNumber);
+        }
+        })   
+    let results = await launching;
+    for (let i = 1; i < results.length; i++) {
+        console.log(`${i}° Launching, ${results[i]}`);
+    }
+}
+
+doTask();
